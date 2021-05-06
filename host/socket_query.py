01e3222d34b86e9fedd688a8626e058ff56fb8c3
mengdie101 <66658144+mengdie101@users.noreply.github.com>
2021-05-06 10:52:31 +0800

    Update socket_query.py

--- host/socket_query.py
+++ host/socket_query.py
@@ -36,9 +36,9 @@ def get_now_date_str(format_string="%Y-%m-%d %H:%M:%S"):#"%Y-%m-%d %H:%M:%S"
     bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
     str_date = bj_dt.strftime(format_string)
     return str_date
-
+hostLocation = r"hosts"
 def output_hosts():
-    with open('host/hosts.txt', 'w') as f:
+    with open(hostLocation, "r") as f:
         f.write('# GitHub Start \n')
         f.write('# Last update at %s (Beijing Time)\n'%(get_now_date_str()))
         for ip, domain in gen_host():
